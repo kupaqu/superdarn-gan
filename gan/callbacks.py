a9cd60ec6769104f13b839f1e3d3584e4bb7f82d
Буда Вампилов <39209065+kupaqu@users.noreply.github.com>
2024-09-04 18:54:24 +0900

    Update callbacks.py

--- gan/callbacks.py
+++ gan/callbacks.py
@@ -13,27 +13,22 @@ class SaveCheckpoint(tf.keras.callbacks.Callback):
         self.model.g.save(os.path.join(epoch_dir, f'g.keras'))
         self.model.d.save(os.path.join(epoch_dir, f'd.keras'))
         self._plot_predictions(
-            plot_path=os.path.join(epoch_dir, f'predictions.jpeg'),
+            plot_path=os.path.join(epoch_dir, f'val_predictions.jpeg'),
             dataset=self.validation_data
         )
         self._plot_predictions(
-            plot_path=os.path.join(epoch_dir, f'predictions.jpeg'),
+            plot_path=os.path.join(epoch_dir, f'train_predictions.jpeg'),
             dataset=self.training_data
         )
 
     def on_train_end(self, logs):
-        epoch_dir = os.path.join(self.dst_dir, 'train_result')
-        os.makedirs(epoch_dir, exist_ok=True)
-        self.model.g.save(os.path.join(epoch_dir, f'g.keras'))
-        self.model.d.save(os.path.join(epoch_dir, f'd.keras'))
-        self._plot_predictions(os.path.join(epoch_dir, f'predictions.jpeg'))
+        self.on_epoch_end('end')
 
     
     def _plot_predictions(self, plot_path, dataset, n_examples=7):
-        numpy_iterator = dataset.as_numpy_iterator()
         figure, axis = plt.subplots(2, n_examples)
 
-        x, y = numpy_iterator.next()
+        x, y = dataset[0]
         x = x[:, :, :, 0:1] * x[:, :, :, 1:2] # канал p_l*qflg
 
         ### предсказания
